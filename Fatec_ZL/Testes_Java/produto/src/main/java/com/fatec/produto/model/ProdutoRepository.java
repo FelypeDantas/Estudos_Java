a66d40fe43fa15341f643e60dcdeaf65ebe09766
Felype <felyped03@gmail.com>
2026-04-03 20:35:14 -0300

    Update ProdutoRepository.java

--- Fatec_ZL/Testes_Java/produto/src/main/java/com/fatec/produto/model/ProdutoRepository.java
+++ Fatec_ZL/Testes_Java/produto/src/main/java/com/fatec/produto/model/ProdutoRepository.java
@@ -1,9 +1,21 @@
-package com.fatec.produto.model;
+package com.fatec.produto.repository;
 
+import com.fatec.produto.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProdutoRepository extends JpaRepository <Produto, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    // Buscar por descrição (contendo texto)
+    List<Produto> findByDescricaoContainingIgnoreCase(String descricao);
+
+    // Buscar por categoria
+    List<Produto> findByCategoriaIgnoreCase(String categoria);
+
+    // Buscar produtos com estoque abaixo de um valor
+    List<Produto> findByQuantidadeNoEstoqueLessThan(int quantidade);
 
 }
