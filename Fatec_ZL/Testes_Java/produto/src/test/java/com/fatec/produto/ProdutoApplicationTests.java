94ac19b0b258f9fa7fb785c0e22e6d00cc1a6652
Felype <felyped03@gmail.com>
2026-04-03 20:37:22 -0300

    Update ProdutoApplicationTests.java

--- Fatec_ZL/Testes_Java/produto/src/test/java/com/fatec/produto/ProdutoApplicationTests.java
+++ Fatec_ZL/Testes_Java/produto/src/test/java/com/fatec/produto/ProdutoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.fatec.produto;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class ProdutoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    @DisplayName("Deve carregar o contexto da aplicação sem erros")
+    void contextLoads() {
+        assertDoesNotThrow(() -> {
+            // Se o contexto subir sem exceções, o teste passa
+        });
+    }
 }
